99fd6e1b27cfefaf27da5d6efb84c802337021ff
DESKTOP-U94E9D8\Win <1>
2023-05-18 16:46:24 +0200

    Dodate nove stranice i style.css
    dodata detaljna-pretraga html, footer html, header html, login html, naslovna html, promena-lozinke html, registracija html, rezultat-pretrage html i glavni style.css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegistracijaComponent } from './registracija/registracija.component';
+import { PromenaLozinkeComponent } from './promena-lozinke/promena-lozinke.component';
+import { NaslovnaComponent } from './naslovna/naslovna.component';
+import { DetaljnaPretragaComponent } from './detaljna-pretraga/detaljna-pretraga.component';
+import { RezultatPretrageComponent } from './rezultat-pretrage/rezultat-pretrage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LoginComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'registracija', component: RegistracijaComponent},
+  {path: 'promena-lozinke', component: PromenaLozinkeComponent},
+  {path: 'naslovna', component: NaslovnaComponent},
+  {path: 'detaljna-pretraga', component: DetaljnaPretragaComponent},
+  {path: 'rezultat-pretrage', component: RezultatPretrageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NaslovnaComponent } from './naslovna/naslovna.component';
+import { LoginComponent } from './login/login.component';
+import { RegistracijaComponent } from './registracija/registracija.component';
+import { PromenaLozinkeComponent } from './promena-lozinke/promena-lozinke.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { RezultatPretrageComponent } from './rezultat-pretrage/rezultat-pretrage.component';
+import { DetaljnaPretragaComponent } from './detaljna-pretraga/detaljna-pretraga.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NaslovnaComponent,
+    LoginComponent,
+    RegistracijaComponent,
+    PromenaLozinkeComponent,
+    HeaderComponent,
+    FooterComponent,
+    RezultatPretrageComponent,
+    DetaljnaPretragaComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
